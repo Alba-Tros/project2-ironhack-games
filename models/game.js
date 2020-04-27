92580fdca33c5e01149fa9160237a90a1bd7a5a0
Marcia Furtado <marciacristianef@gmail.com>
2020-04-27 15:35:41 +0200

    game model and initial seeds

---
+++ models/game.js
@@ -0,0 +1,22 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const gameSchema = new Schema(
+    {
+      title: String,
+      gameUrl: String,
+      gitHubUrl: String,
+      author: String,
+      bootcamp: String,
+      description: String,
+      gameScreenshot: [{String}],
+      rating: Number
+    },
+    {
+      timestamps: { createdAt: "created_at", updatedAt: "updated_at" }
+    }
+);
+
+const Game = mongoose.model('Game', gameSchema);
+
+module.exports = Game;
