c01dab63e5b53c6d6a11e4d67124f5453b8d51d0
Benjamin Chimoy <benjaminchimoy@benjamins-mbp.localdomain>
2020-04-28 18:25:07 +0200

    images upload

--- models/Game.js
+++ models/Game.js
@@ -2,21 +2,23 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const gameSchema = new Schema(
-    {
-      title: String,
-      gameUrl: String,
-      gitHubUrl: String,
-      author: String,
-      bootcamp: String,
-      description: String,
-      gameScreenshot: [{String}],
-      rating: Number
-    },
-    {
-      timestamps: { createdAt: "created_at", updatedAt: "updated_at" }
-    }
+  {
+    title: String,
+    gameUrl: String,
+    gitHubUrl: String,
+    author: String,
+    bootcamp: String,
+    description: String,
+    gameScreenshot: [{ String }],
+    rating: Number,
+    imgName: String,
+    imgPath: String,
+  },
+  {
+    timestamps: { createdAt: "created_at", updatedAt: "updated_at" },
+  }
 );
 
-const Game = mongoose.model('Game', gameSchema);
+const Game = mongoose.model("Game", gameSchema);
 
 module.exports = Game;
