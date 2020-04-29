3032dda17bb0c3bb7d4c4f5bd1dec8a7a4fe2097
Alba-Tros <samipcpoolberlin@yahoo.fr>
2020-04-29 10:07:29 +0200

    changed the game model name to capitalized in the seeds.js because was having an error

--- bin/seeds.js
+++ bin/seeds.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
-const Game = require("../models/game");
+const Game = require("../models/Game");
 
 mongoose.connect(
     process.env.MONGODB_URI || "mongodb://localhost/project2-ironhackgames",
