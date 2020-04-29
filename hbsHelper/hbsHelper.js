f96c0e9e7b31003b395a7e3844e2d28427687c47
Marcia Furtado <marciacristianef@gmail.com>
2020-04-29 08:49:02 +0200

    Add My Account link and dropdown options

--- hbsHelper/hbsHelper.js
+++
@@ -1,9 +0,0 @@
-// const hbs = require('hbs');
-
-// hbs.registerHelper('userHelper', function(reqUser) {
-//   var markus = "really??";
-//   var map = {};
-  
-//   console.log(reqUser);
-//   return new hbs.SafeString(reqUser.username);
-// }); 
--- public/javascripts/checkIfLoggedIn.js
+++ public/javascripts/checkIfLoggedIn.js
@@ -3,6 +3,7 @@ axios.get('/get-user').then(response => {
   // document.getElementById('markus').innerText = response.data.loggedInUser.username;
   if(response.data.loggedInUser) {
     document.getElementById('logout').style.display = 'block';
+    document.getElementById('my-account').style.display = 'block';
     document.getElementById('login').style.display = 'none';
     document.getElementById('signup').style.display = 'none';
   }
