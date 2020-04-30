6a951f75b47efddb8fd07bb838db771a7d996997
Benjamin Chimoy <benjaminchimoy@Benjamins-MBP.home>
2020-04-30 12:55:24 +0200

    route private for evrythig

--- routes/auth-routes.js
+++ routes/auth-routes.js
@@ -3,6 +3,7 @@ const express = require("express");
 const router = express.Router();
 const passport = require("passport");
 const ensureLogin = require("connect-ensure-login");
+const Game = require("../models/Game");
 
 // User model
 const User = require("../models/user");
@@ -12,107 +13,113 @@ const bcrypt = require("bcrypt");
 const bcryptSalt = 10;
 
 router.get("/signup", (req, res, next) => {
-    res.render("auth/signup");
+  res.render("auth/signup");
 });
 
 // getting info from the form username and password
 
 router.post("/signup", (req, res, next) => {
-    const username = req.body.username;
-    const password = req.body.password;
+  const username = req.body.username;
+  const password = req.body.password;
 
-    if (username === "" || password === "") {
+  if (username === "" || password === "") {
+    res.render("auth/signup", {
+      message: "Please enter a username and a password",
+    });
+    return;
+  } else if (username === "") {
+    res.render("auth/signup", {
+      message: "Please enter a username",
+    });
+    return;
+  } else if (password === "") {
+    res.render("auth/signup", {
+      message: "Please enter a password",
+    });
+    return;
+  }
+
+  // if it allready existsts
+
+  User.findOne({ username })
+    .then((user) => {
+      if (user !== null) {
         res.render("auth/signup", {
-            message: "Please enter a username and a password"
+          message: "This username already exists",
         });
         return;
-    } else if (username === "") {
-        res.render("auth/signup", {
-            message: "Please enter a username"
-        });
-        return;
-    } else if (password === "") {
-        res.render("auth/signup", {
-            message: "Please enter a password"
-        });
-        return;
-    }
+      }
 
-    // if it allready existsts
+      const salt = bcrypt.genSaltSync(bcryptSalt);
+      const hashPass = bcrypt.hashSync(password, salt);
 
-    User.findOne({ username })
-        .then(user => {
-            if (user !== null) {
-                res.render("auth/signup", {
-                    message: "This username already exists"
-                });
-                return;
-            }
+      const newUser = new User({
+        username,
+        password: hashPass,
+      });
 
-            const salt = bcrypt.genSaltSync(bcryptSalt);
-            const hashPass = bcrypt.hashSync(password, salt);
-
-            const newUser = new User({
-                username,
-                password: hashPass
-            });
-
-            newUser.save(err => {
-                if (err) {
-                    res.render("auth/signup", {
-                        message: "Something went wrong"
-                    });
-                } else {
-                    // privat page !
-                    res.redirect("/private");
-                }
-            });
-        })
-        .catch(error => {
-            next(error);
-        });
+      newUser.save((err) => {
+        if (err) {
+          res.render("auth/signup", {
+            message: "Something went wrong",
+          });
+        } else {
+          // privat page !
+          res.redirect("/private");
+        }
+      });
+    })
+    .catch((error) => {
+      next(error);
+    });
 });
 
 router.get("/login", (req, res, next) => {
-    res.render("auth/login", { message: req.flash("error") });
+  res.render("auth/login", { message: req.flash("error") });
 });
 
 router.post(
-    "/login",
-    passport.authenticate("local", {
-        // private page
-        successRedirect: "/private",
-        failureRedirect: "/login",
-        failureFlash: true,
-        passReqToCallback: true
-    })
+  "/login",
+  passport.authenticate("local", {
+    // private page
+    successRedirect: "/private",
+    failureRedirect: "/login",
+    failureFlash: true,
+    passReqToCallback: true,
+  })
 );
 
 router.get("/logout", (req, res) => {
-    req.logout();
-    res.redirect("/login");
+  req.logout();
+  res.redirect("/login");
 });
 
-router.get("/account-settings", (req, res) => {
-    res.render("account-settings");
-});
+//router.get("/account-settings", (req, res) => {
+//  Game.findById(req.user._id).then((games) => {
+//   res.render("account-settings", { gamesList: games });
+// });
+// });
 
-// work on this
 router.get("/private", ensureLogin.ensureLoggedIn(), (req, res) => {
-    res.render("private", { user: req.user });
+  Game.find({ owner: req.user._id }).then((games) => {
+    console.log(games);
+    console.log(req.user);
+    res.render("private", { gamesList: games, user: req.user });
+  });
 });
+
 router.get("/auth/github", passport.authenticate("github"));
 router.get(
-    "/auth/github/callback",
-    passport.authenticate("github", {
-        successRedirect: "/private",
-        failureRedirect: "/"
-    })
+  "/auth/github/callback",
+  passport.authenticate("github", {
+    successRedirect: "/private",
+    failureRedirect: "/",
+  })
 );
 
 router.get("/get-user", (req, res) => {
-    const user = req.user;
-    return res.status(200).json({ loggedInUser: user });
+  const user = req.user;
+  return res.status(200).json({ loggedInUser: user });
 });
 
 module.exports = router;
