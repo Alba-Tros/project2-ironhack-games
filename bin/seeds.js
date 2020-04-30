f73d68def410d6da0e167f435dde0856690f51a2
Marcia Furtado <marciacristianef@gmail.com>
2020-04-30 12:45:07 +0200

    home page pretty much done

--- bin/seeds.js
+++ bin/seeds.js
@@ -63,7 +63,7 @@ const games = [
             "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
     },
     {
-        title: "Taco cat",
+        title: "Taco Cat",
         gameUrl: "https://bleepblopblorpblopbloop.github.io/tacocat/",
         gitHubUrl: "https://github.com/bleepblopblorpblopbloop",
         bootcamp: "October/2019",
